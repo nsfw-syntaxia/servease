27bed52cc4aab3676652882b933d77a52ebae041
Shanii <shanraboy11@gmail.com>
2025-06-18 22:26:31 +0800

    style(register page): export from figma - Raboy

--- ServEase/src/app/page.tsx
+++ ServEase/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/signin");
+  // This correctly points to the /register route.
+  redirect("/register");
 }
