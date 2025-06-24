3c6395549a9b0d4273d02d7e5415c805f3563217
Shanii <shanraboy11@gmail.com>
2025-06-24 11:20:45 +0800

    fix: auth error again huhu - Raboy

--- ServEase/src/app/signin/actions.ts
+++ ServEase/src/app/signin/actions.ts
@@ -7,7 +7,7 @@ export async function login(formData: FormData) {
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
-  const supabase = createClient();
+  const supabase = await createClient();
 
   const { error } = await supabase.auth.signInWithPassword({
     email,
