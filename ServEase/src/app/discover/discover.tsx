cebe69341505d6f4f2191c48d6474bb80e0dea8d
rexi <4611trixiedolera@gmail.com>
2025-06-29 20:31:15 +0800

    fix: redirect and add anims in discover page

--- ServEase/src/app/discover/discover.tsx
+++ ServEase/src/app/discover/discover.tsx
@@ -8,9 +8,34 @@ import styles from "../styles/DiscoverPage.module.css";
 
 const DiscoverPage: NextPage = () => {
   const router = useRouter();
-  const onLinkContainerClick = useCallback(() => {
-    console.log("Link clicked!");
-  }, []);
+
+  const onPersonalBeautyClick = useCallback(() => {
+    router.push("/discover/categories/personal-beauty-and-care");
+  }, [router]);
+
+  const onHealthMedicalClick = useCallback(() => {
+    router.push("/discover/categories/health-and-medical");
+  }, [router]);
+
+  const onFitnessSportsClick = useCallback(() => {
+    router.push("/discover/categories/fitness-and-sports");
+  }, [router]);
+
+  const onEducationTutoringClick = useCallback(() => {
+    router.push("/discover/categories/education-and-tutoring");
+  }, [router]);
+
+  const onRepairTechnicalClick = useCallback(() => {
+    router.push("/discover/categories/repair-and-technical");
+  }, [router]);
+
+  const onFoodBeveragesClick = useCallback(() => {
+    router.push("/discover/categories/food-and-beverages");
+  }, [router]);
+
+  const onMiscellaneousClick = useCallback(() => {
+    router.push("/discover/categories/miscellaneous");
+  }, [router]);
 
   return (
     <div className={styles.discover1Parent}>
@@ -70,27 +95,13 @@ const DiscoverPage: NextPage = () => {
         </div>
 
         <div className={styles.whatweofferbox} />
-        <div className={styles.background}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
-            <div className={styles.miscellaneous}>Miscellaneous</div>
-            <div className={styles.chevronRight}>
-              <Image
-                className={styles.icon}
-                width={6.8}
-                height={13.5}
-                sizes="100vw"
-                alt=""
-                src="Chevron right.svg"
-              />
-            </div>
-          </div>
-        </div>
         <b className={styles.ourServices}>
           <span>{`Our `}</span>
           <span className={styles.services}>Services</span>
         </b>
-        <div className={styles.background1}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
+
+        <div className={styles.background1} onClick={onPersonalBeautyClick}>
+          <div className={styles.link}>
             <div className={styles.personalBeautyAnd}>
               Personal Beauty and Care
             </div>
@@ -106,8 +117,9 @@ const DiscoverPage: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className={styles.background2}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
+
+        <div className={styles.background2} onClick={onHealthMedicalClick}>
+          <div className={styles.link}>
             <div className={styles.healthAndMedical}>
               Health and Medical Services
             </div>
@@ -123,8 +135,9 @@ const DiscoverPage: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className={styles.background3}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
+
+        <div className={styles.background3} onClick={onFitnessSportsClick}>
+          <div className={styles.link}>
             <div className={styles.fitnessAndSports}>Fitness and Sports</div>
             <div className={styles.chevronRight}>
               <Image
@@ -138,8 +151,9 @@ const DiscoverPage: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className={styles.background4}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
+
+        <div className={styles.background4} onClick={onEducationTutoringClick}>
+          <div className={styles.link}>
             <div className={styles.educationAndTutoring}>
               Education and Tutoring
             </div>
@@ -156,8 +170,9 @@ const DiscoverPage: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className={styles.background5}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
+
+        <div className={styles.background5} onClick={onRepairTechnicalClick}>
+          <div className={styles.link}>
             <div className={styles.repairAndTechnical}>
               Repair and Technical Services
             </div>
@@ -173,8 +188,9 @@ const DiscoverPage: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className={styles.background6}>
-          <div className={styles.link} onClick={onLinkContainerClick}>
+
+        <div className={styles.background6} onClick={onFoodBeveragesClick}>
+          <div className={styles.link}>
             <div className={styles.foodAndBeverages}>Food and Beverages</div>
             <div className={styles.chevronRight}>
               <Image
@@ -184,6 +200,22 @@ const DiscoverPage: NextPage = () => {
                 sizes="100vw"
                 alt=""
                 src="/Chevron right.svg"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.background} onClick={onMiscellaneousClick}>
+          <div className={styles.link}>
+            <div className={styles.miscellaneous}>Miscellaneous</div>
+            <div className={styles.chevronRight}>
+              <Image
+                className={styles.icon}
+                width={6.8}
+                height={13.5}
+                sizes="100vw"
+                alt=""
+                src="Chevron right.svg"
               />
             </div>
           </div>
