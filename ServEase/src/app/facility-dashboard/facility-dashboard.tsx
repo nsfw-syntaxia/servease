b6c54c47e16668d07375fd2222898495135120a7
rexi <4611trixiedolera@gmail.com>
2025-07-02 00:00:10 +0800

    feat: export from figma

---
+++ ServEase/src/app/facility-dashboard/facility-dashboard.tsx
@@ -0,0 +1,396 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "./index.module.css";
+
+const DashboardFacility: NextPage = () => {
+  return (
+    <div className={styles.dashboardFacility}>
+      <div className={styles.joinUsWrapper}>
+        <div className={styles.joinUs}>
+          <div className={styles.heroImageWrapper}>
+            <div className={styles.heroImage}>
+              <div className={styles.manageYour}>Manage your</div>
+              <div className={styles.services}>Services</div>
+              <Image
+                className={styles.heroImageChild}
+                width={95}
+                height={15}
+                sizes="100vw"
+                alt=""
+                src="Group 126.svg"
+              />
+            </div>
+          </div>
+          <div className={styles.navigation}>
+            <Image
+              className={styles.serveaseLogoAlbumCover3}
+              width={40}
+              height={40}
+              sizes="100vw"
+              alt=""
+              src="Servease Logo (Album Cover) (3) 1.png"
+            />
+            <div className={styles.servease}>
+              <span className={styles.serv}>serv</span>
+              <b>ease</b>
+            </div>
+            <div className={styles.navigationChild} />
+            <div className={styles.homeParent}>
+              <div className={styles.home}>Home</div>
+              <div className={styles.home}>Discover</div>
+              <div className={styles.contactUs}>Contact Us</div>
+            </div>
+            <div className={styles.navigationItem} />
+            <div className={styles.genericAvatar}>
+              <Image
+                className={styles.avatarPlaceholderIcon}
+                width={28.2}
+                height={25.6}
+                sizes="100vw"
+                alt=""
+                src="Avatar Placeholder.svg"
+              />
+            </div>
+          </div>
+          <div className={styles.dateDayXx}>
+            Date (Day, XX Month, Year, Time)
+          </div>
+          <div className={styles.overview}>Overview</div>
+          <div className={styles.joinUsInner}>
+            <div className={styles.groupWrapper}>
+              <div className={styles.groupWrapper}>
+                <div className={styles.groupWrapper}>
+                  <div className={styles.groupWrapper}>
+                    <div className={styles.heroImage1}>
+                      <Image
+                        className={styles.avatarIcon}
+                        width={70}
+                        height={70}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Image
+            className={styles.avatarIcon1}
+            width={60}
+            height={60}
+            sizes="100vw"
+            alt=""
+            src="Avatar.svg"
+          />
+          <div className={styles.php1573500}>Php 15,735.00</div>
+          <div className={styles.todaysTotalRevenue}>Today’s Total Revenue</div>
+          <div className={styles.joinUsChild}>
+            <div className={styles.groupDiv}>
+              <div className={styles.groupDiv}>
+                <div className={styles.groupDiv}>
+                  <div className={styles.groupDiv}>
+                    <div className={styles.heroImage2}>
+                      <Image
+                        className={styles.avatarIcon}
+                        width={70}
+                        height={70}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Image
+            className={styles.avatarIcon3}
+            width={60}
+            height={60}
+            sizes="100vw"
+            alt=""
+            src="Avatar.svg"
+          />
+          <div className={styles.k}>4.78K</div>
+          <div className={styles.totalLikes}>Total Likes</div>
+          <div className={styles.joinUsInner1}>
+            <div className={styles.groupDiv}>
+              <div className={styles.groupDiv}>
+                <div className={styles.groupDiv}>
+                  <div className={styles.groupDiv}>
+                    <div className={styles.heroImage2}>
+                      <Image
+                        className={styles.avatarIcon}
+                        width={70}
+                        height={70}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Image
+            className={styles.avatarIcon5}
+            width={60}
+            height={60}
+            sizes="100vw"
+            alt=""
+            src="Avatar.svg"
+          />
+          <div className={styles.div}>4.2</div>
+          <div className={styles.div1}>/5.0</div>
+          <div className={styles.ratings}>Ratings</div>
+          <div className={styles.card}>
+            <Image
+              className={styles.vuesaxlinearcardIcon}
+              width={27}
+              height={27}
+              sizes="100vw"
+              alt=""
+              src="vuesax/linear/card.svg"
+            />
+          </div>
+          <div className={styles.joinUsInner2}>
+            <div className={styles.groupWrapper}>
+              <div className={styles.groupWrapper}>
+                <div className={styles.groupWrapper}>
+                  <div className={styles.groupWrapper}>
+                    <div className={styles.heroImage1}>
+                      <Image
+                        className={styles.avatarIcon}
+                        width={70}
+                        height={70}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Image
+            className={styles.avatarIcon7}
+            width={60}
+            height={60}
+            sizes="100vw"
+            alt=""
+            src="Avatar.svg"
+          />
+          <div className={styles.div2}>15</div>
+          <div className={styles.totalCompletedAppointments}>
+            Total Completed Appointments
+          </div>
+          <div className={styles.star}>
+            <Image
+              className={styles.vuesaxlinearcardIcon}
+              width={27}
+              height={27}
+              sizes="100vw"
+              alt=""
+              src="vuesax/linear/star.svg"
+            />
+          </div>
+          <div className={styles.heart}>
+            <Image
+              className={styles.vuesaxlinearcardIcon}
+              width={27}
+              height={27}
+              sizes="100vw"
+              alt=""
+              src="vuesax/linear/heart.svg"
+            />
+          </div>
+          <div className={styles.noteText}>
+            <Image
+              className={styles.vuesaxlinearcardIcon}
+              width={27}
+              height={27}
+              sizes="100vw"
+              alt=""
+              src="vuesax/linear/note-text.svg"
+            />
+          </div>
+          <b className={styles.upcomingAppointments}>
+            <span className={styles.upcomingAppointmentsTxtContainer}>
+              <span>Upcoming</span>
+              <span className={styles.appointments}> Appointments</span>
+            </span>
+          </b>
+          <div className={styles.groupParent}>
+            <div className={styles.groupParent1}>
+              <div className={styles.heroImageParent}>
+                <div className={styles.heroImage5}>
+                  <Image
+                    className={styles.avatarIcon8}
+                    width={90}
+                    height={90}
+                    sizes="100vw"
+                    alt=""
+                    src="Avatar.svg"
+                  />
+                  <div className={styles.clientName}>Client Name</div>
+                  <div className={styles.service}>Service</div>
+                </div>
+                <div className={styles.heroImage6}>
+                  <Image
+                    className={styles.calendarMonthIcon}
+                    width={24}
+                    height={24}
+                    sizes="100vw"
+                    alt=""
+                    src="calendar_month.svg"
+                  />
+                </div>
+              </div>
+              <div className={styles.vectorParent}>
+                <Image
+                  className={styles.vectorIcon}
+                  width={20}
+                  height={20}
+                  sizes="100vw"
+                  alt=""
+                  src="Vector.svg"
+                />
+                <div className={styles.pm}>1:00 PM</div>
+              </div>
+              <div className={styles.wedJune30}>Wed, June 30</div>
+            </div>
+            <div className={styles.groupWrapper9}>
+              <div className={styles.groupWrapper10}>
+                <div className={styles.groupWrapper10}>
+                  <div className={styles.groupWrapper10}>
+                    <div className={styles.groupWrapper10}>
+                      <div className={styles.heroImageParent}>
+                        <div className={styles.heroImage5}>
+                          <Image
+                            className={styles.avatarIcon8}
+                            width={90}
+                            height={90}
+                            sizes="100vw"
+                            alt=""
+                            src="Avatar.svg"
+                          />
+                          <div className={styles.clientName}>Client Name</div>
+                          <div className={styles.service}>Service</div>
+                        </div>
+                        <div className={styles.heroImage6}>
+                          <Image
+                            className={styles.calendarMonthIcon}
+                            width={24}
+                            height={24}
+                            sizes="100vw"
+                            alt=""
+                            src="calendar_month.svg"
+                          />
+                        </div>
+                      </div>
+                      <div className={styles.vectorGroup}>
+                        <Image
+                          className={styles.vectorIcon1}
+                          width={16.7}
+                          height={16.7}
+                          sizes="100vw"
+                          alt=""
+                          src="Vector.svg"
+                        />
+                        <div className={styles.pm1}>1:00 PM</div>
+                      </div>
+                      <div className={styles.wedJune30}>Wed, June 30</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.button}>
+            <div className={styles.viewAll}>View All</div>
+          </div>
+          <b className={styles.pendingAppointments}>
+            <span className={styles.upcomingAppointmentsTxtContainer}>
+              <span>Pending</span>
+              <span className={styles.appointments}> Appointments</span>
+            </span>
+          </b>
+          <div className={styles.button1}>
+            <div className={styles.viewAll}>View All</div>
+          </div>
+          <div className={styles.pending}>
+            <div className={styles.shanMichaelRaboy}>Shan Michael Raboy</div>
+            <div className={styles.haircut}>Haircut</div>
+            <div className={styles.div3}>13:00</div>
+            <div className={styles.november1}>November 1</div>
+          </div>
+          <div className={styles.clientName2}>Client Name</div>
+          <div className={styles.service2}>Service</div>
+          <div className={styles.time}>Time</div>
+          <div className={styles.date}>Date</div>
+          <div className={styles.pending1}>
+            <div className={styles.gioChristianMacatual}>
+              Gio Christian Macatual
+            </div>
+            <div className={styles.haircut}>Haircut</div>
+            <div className={styles.div3}>13:00</div>
+            <div className={styles.november1}>December 2</div>
+          </div>
+          <div className={styles.pending2}>
+            <div className={styles.gioChristianMacatual}>Trixie Dolera</div>
+            <div className={styles.haircut}>Haircut</div>
+            <div className={styles.div3}>13:00</div>
+            <div className={styles.november1}>January 13</div>
+          </div>
+          <div className={styles.pending3}>
+            <div className={styles.gioChristianMacatual}>Maica Eupinado</div>
+            <div className={styles.haircut}>Haircut</div>
+            <div className={styles.div3}>13:00</div>
+            <div className={styles.november1}>September 25</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footerChild} />
+        <div className={styles.yourTrustedPlatform}>
+          Your trusted platform to discover, book, and manage local
+          services—anytime, anywhere.
+        </div>
+        <b className={styles.contactUs1}>Contact Us</b>
+        <div className={styles.supportserveasecom}>support@servease.com</div>
+        <div className={styles.div7}>+63 996 3175 214</div>
+        <b className={styles.support}>Support</b>
+        <div className={styles.faqs}>FAQs</div>
+        <div className={styles.privacyPolicy}>Privacy Policy</div>
+        <div className={styles.termsConditions}>{`Terms & Conditions`}</div>
+        <div className={styles.aboutUs}>About Us</div>
+        <b className={styles.quickLinks}>Quick Links</b>
+        <b className={styles.servease1}>servease</b>
+        <div className={styles.home1}>Home</div>
+        <div className={styles.discover1}>Discover</div>
+        <div className={styles.createAnAccount}>Create an Account</div>
+        <div className={styles.lineParent}>
+          <div className={styles.groupChild} />
+          <div className={styles.servease2025}>
+            servease 2025 © All rights reserved
+          </div>
+        </div>
+        <Image
+          className={styles.serveaseLogoAlbumCover31}
+          width={40}
+          height={40}
+          sizes="100vw"
+          alt=""
+          src="Servease Logo (Album Cover) (3) 2.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardFacility;
