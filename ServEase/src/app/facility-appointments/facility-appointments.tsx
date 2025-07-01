8da38642d55f8241925167329db0142156b29f7f
rexi <4611trixiedolera@gmail.com>
2025-07-02 00:21:59 +0800

    feat: export from figma

---
+++ ServEase/src/app/facility-appointments/facility-appointments.tsx
@@ -0,0 +1,314 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "../styles/facility-appointments.module.css";
+
+const FacilityAppointments: NextPage = () => {
+  return (
+    <div className={styles.appointments}>
+      <div className={styles.joinUsParent}>
+        <div className={styles.joinUs}>
+          <div className={styles.joinUsChild} />
+          <div className={styles.paraContent} />
+          <div className={styles.navigation}>
+            <Image
+              className={styles.serveaseLogoAlbumCover3}
+              width={40}
+              height={40}
+              sizes="100vw"
+              alt=""
+              src="Servease Logo (Album Cover) (3) 1.png"
+            />
+            <div className={styles.servease}>
+              <span className={styles.serv}>serv</span>
+              <b>ease</b>
+            </div>
+            <div className={styles.navigationChild} />
+            <div className={styles.homeParent}>
+              <div className={styles.home}>Home</div>
+              <div className={styles.home}>Discover</div>
+              <div className={styles.contactUs}>Contact Us</div>
+            </div>
+            <div className={styles.navigationItem} />
+            <div className={styles.genericAvatar}>
+              <Image
+                className={styles.avatarPlaceholderIcon}
+                width={28.2}
+                height={25.6}
+                sizes="100vw"
+                alt=""
+                src="Avatar Placeholder.svg"
+              />
+            </div>
+          </div>
+          <b className={styles.appointments1}>Appointments</b>
+          <div className={styles.joinUsInner}>
+            <div className={styles.groupParent}>
+              <div className={styles.groupParent}>
+                <div className={styles.groupParent}>
+                  <div className={styles.heroImageParent}>
+                    <div className={styles.heroImage}>
+                      <Image
+                        className={styles.avatarIcon}
+                        width={110}
+                        height={110}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                      <div className={styles.clientName}>Client Name</div>
+                      <div className={styles.service}>Service</div>
+                    </div>
+                    <div className={styles.heroImage1}>
+                      <Image
+                        className={styles.vectorIcon}
+                        width={21}
+                        height={21}
+                        sizes="100vw"
+                        alt=""
+                        src="Vector.svg"
+                      />
+                      <Image
+                        className={styles.calendarMonthIcon}
+                        width={26}
+                        height={26}
+                        sizes="100vw"
+                        alt=""
+                        src="calendar_month.svg"
+                      />
+                      <Image
+                        className={styles.avatarIcon1}
+                        width={21}
+                        height={21}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.pm}>1:00 PM</div>
+                  <div className={styles.calendarMonthParent}>
+                    <Image
+                      className={styles.calendarMonthIcon1}
+                      width={19.5}
+                      height={21.7}
+                      sizes="100vw"
+                      alt=""
+                      src="calendar_month.svg"
+                    />
+                    <div className={styles.wedJune30}>Wed, June 30</div>
+                  </div>
+                  <div className={styles.confirmed}>Confirmed</div>
+                </div>
+              </div>
+              <div className={styles.viewDetailsParent}>
+                <div className={styles.viewDetails}>View Details</div>
+                <Image
+                  className={styles.svgIcon}
+                  width={17}
+                  height={16}
+                  sizes="100vw"
+                  alt=""
+                  src="SVG.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.groupDiv}>
+            <div className={styles.groupParent}>
+              <div className={styles.groupParent}>
+                <div className={styles.groupParent}>
+                  <div className={styles.heroImageParent}>
+                    <div className={styles.heroImage}>
+                      <Image
+                        className={styles.avatarIcon}
+                        width={110}
+                        height={110}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                      <div className={styles.clientName}>Client Name</div>
+                      <div className={styles.service}>Service</div>
+                    </div>
+                    <div className={styles.heroImage1}>
+                      <Image
+                        className={styles.vectorIcon}
+                        width={21}
+                        height={21}
+                        sizes="100vw"
+                        alt=""
+                        src="Vector.svg"
+                      />
+                      <Image
+                        className={styles.calendarMonthIcon}
+                        width={26}
+                        height={26}
+                        sizes="100vw"
+                        alt=""
+                        src="calendar_month.svg"
+                      />
+                      <Image
+                        className={styles.avatarIcon1}
+                        width={21}
+                        height={21}
+                        sizes="100vw"
+                        alt=""
+                        src="Avatar.svg"
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.pm}>1:00 PM</div>
+                  <div className={styles.calendarMonthParent}>
+                    <Image
+                      className={styles.calendarMonthIcon1}
+                      width={19.5}
+                      height={21.7}
+                      sizes="100vw"
+                      alt=""
+                      src="calendar_month.svg"
+                    />
+                    <div className={styles.wedJune30}>Wed, June 30</div>
+                  </div>
+                  <div className={styles.confirmed}>Confirmed</div>
+                </div>
+              </div>
+              <div className={styles.viewDetailsParent}>
+                <div className={styles.viewDetails}>View Details</div>
+                <Image
+                  className={styles.svgIcon}
+                  width={17}
+                  height={16}
+                  sizes="100vw"
+                  alt=""
+                  src="SVG.svg"
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.button}>
+            <div className={styles.cancelled}>Cancelled</div>
+          </div>
+          <div className={styles.button1}>
+            <div className={styles.cancelled}>Completed</div>
+          </div>
+          <div className={styles.button2}>
+            <div className={styles.cancelled}>Pending</div>
+            <div className={styles.button3}>
+              <div className={styles.upcoming}>Upcoming</div>
+            </div>
+          </div>
+          <div className={styles.footer}>
+            <div className={styles.footerChild} />
+            <div className={styles.yourTrustedPlatform}>
+              Your trusted platform to discover, book, and manage local
+              services—anytime, anywhere.
+            </div>
+            <b className={styles.contactUs1}>Contact Us</b>
+            <div className={styles.supportserveasecom}>
+              support@servease.com
+            </div>
+            <div className={styles.div}>+63 996 3175 214</div>
+            <b className={styles.support}>Support</b>
+            <div className={styles.faqs}>FAQs</div>
+            <div className={styles.privacyPolicy}>Privacy Policy</div>
+            <div className={styles.termsConditions}>{`Terms & Conditions`}</div>
+            <div className={styles.aboutUs}>About Us</div>
+            <b className={styles.quickLinks}>Quick Links</b>
+            <b className={styles.servease1}>servease</b>
+            <div className={styles.home1}>Home</div>
+            <div className={styles.discover1}>Discover</div>
+            <div className={styles.createAnAccount}>Create an Account</div>
+            <div className={styles.lineParent}>
+              <div className={styles.groupChild} />
+              <div className={styles.servease2025}>
+                servease 2025 © All rights reserved
+              </div>
+            </div>
+            <Image
+              className={styles.serveaseLogoAlbumCover31}
+              width={40}
+              height={40}
+              sizes="100vw"
+              alt=""
+              src="Servease Logo (Album Cover) (3) 2.png"
+            />
+          </div>
+        </div>
+        <div className={styles.groupWrapper1}>
+          <div className={styles.groupParent}>
+            <div className={styles.groupParent}>
+              <div className={styles.groupParent}>
+                <div className={styles.heroImageParent}>
+                  <div className={styles.heroImage}>
+                    <Image
+                      className={styles.avatarIcon}
+                      width={110}
+                      height={110}
+                      sizes="100vw"
+                      alt=""
+                      src="Avatar.svg"
+                    />
+                    <div className={styles.clientName}>Client Name</div>
+                    <div className={styles.service}>Service</div>
+                  </div>
+                  <div className={styles.heroImage1}>
+                    <Image
+                      className={styles.vectorIcon}
+                      width={21}
+                      height={21}
+                      sizes="100vw"
+                      alt=""
+                      src="Vector.svg"
+                    />
+                    <Image
+                      className={styles.calendarMonthIcon}
+                      width={26}
+                      height={26}
+                      sizes="100vw"
+                      alt=""
+                      src="calendar_month.svg"
+                    />
+                    <Image
+                      className={styles.avatarIcon1}
+                      width={21}
+                      height={21}
+                      sizes="100vw"
+                      alt=""
+                      src="Avatar.svg"
+                    />
+                  </div>
+                </div>
+                <div className={styles.pm}>1:00 PM</div>
+                <div className={styles.calendarMonthParent}>
+                  <Image
+                    className={styles.calendarMonthIcon1}
+                    width={19.5}
+                    height={21.7}
+                    sizes="100vw"
+                    alt=""
+                    src="calendar_month.svg"
+                  />
+                  <div className={styles.wedJune30}>Wed, June 30</div>
+                </div>
+                <div className={styles.confirmed}>Confirmed</div>
+              </div>
+            </div>
+            <div className={styles.viewDetailsParent}>
+              <div className={styles.viewDetails}>View Details</div>
+              <Image
+                className={styles.svgIcon}
+                width={17}
+                height={16}
+                sizes="100vw"
+                alt=""
+                src="SVG.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FacilityAppointments;
