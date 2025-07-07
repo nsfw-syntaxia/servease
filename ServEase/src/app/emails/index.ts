050166d5268275ef57592154459e2ccefc739253
Shanii <shanraboy11@gmail.com>
2025-07-07 21:31:21 +0800

    fix: index import directory - Raboy

--- ServEase/src/app/emails/index.ts
+++ ServEase/src/app/emails/index.ts
@@ -1,5 +1,3 @@
-
-
+export { CancellationNoticeToProvider } from "./CancellationNoticeToProvider"
+export { CancellationConfirmationToClient } from "./CancellationConfirmationToClient"
 export { ClientBookingPending } from './ClientBookingPending';
-export { ProviderBookingNotification } from './ProviderBookingNotification';
-export { ClientBookingConfirmation } from './ClientBookingConfirmation';
